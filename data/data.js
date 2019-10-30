b361e80edd0757877a6101720f467d6a7595c427
pankajgouraw <pgouraw@gmail.com>
2019-10-30 11:30:22 +0530

    initial commit

---
+++ data/data.js
@@ -0,0 +1,11 @@
+
+const headerText = 'Multiplication';
+const headerInstructionColor = 'white';
+const min = 500;
+const max = 1000;
+const lowerMin = 0;
+const lowerMax = 200;
+const bg = 'radial-gradient(#4CAF50, #009688)';
+
+
+
